fc3852f24ba7b35d16700410abc97d15f37a5cd8
Caleb335 <belac335@gmail.com>
2020-06-01 23:52:45 +0100

    project setup complete

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function App() {
+  return (
+    <div className='app__base asteroids'>
+      <h3>Reveal your approaches Oh! ye asteroids 🤣 </h3>
+    </div>
+  )
+}
