a3de8441ed947541f200656baf30932356c21bce
Caleb335 <belac335@gmail.com>
2020-07-20 17:29:37 +0100

    refactoring map function

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -28291,17 +28291,97 @@ if ("development" === 'production') {
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = App;
+exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function App() {
-  return _react.default.createElement("div", {
-    className: "app__base asteroids"
-  }, _react.default.createElement("h3", null, "Reveal your approaches Oh! ye asteroids \uD83E\uDD23 "));
-}
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+// API Endpoint && key
+var asteroid_key = "7UpN204mxX9ABjxG3j8quPfoLak8N2WGyZBaY741";
+var asetroid_endpoint = "https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=".concat(asteroid_key);
+
+var App = /*#__PURE__*/function (_React$Component) {
+  _inherits(App, _React$Component);
+
+  var _super = _createSuper(App);
+
+  function App() {
+    var _this;
+
+    _classCallCheck(this, App);
+
+    _this = _super.call(this);
+    _this.state = {
+      asteroids: [],
+      isLoading: false
+    };
+    return _this;
+  } // fetch data from NASA's NEoW API
+
+
+  _createClass(App, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      var _this2 = this;
+
+      // get loading key
+      this.setState({
+        isLoading: true
+      });
+      fetch(asetroid_endpoint).then(function (response) {
+        return response.json;
+      }).then(function (data) {
+        return _this2.setState({
+          asteroids: data.asteroids
+        });
+      });
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      var _this$state = this.state,
+          asteroids = _this$state.asteroids,
+          isLoading = _this$state.isLoading; // perform react conditional rendering
+      // by displaying either a loader or text indicating that data is being fetched
+
+      isLoading ? _react.default.createElement("p", null, "Loading...") : 'err';
+      return _react.default.createElement("div", {
+        className: "app__base asteroids"
+      }, _react.default.createElement("h3", null, "Reveal your approaches Oh! ye asteroids \uD83E\uDD23 "), asteroids.map(function (asteroid) {
+        return _react.default.createElement("div", {
+          key: asteroid.ObjectID
+        }, _react.default.createElement("h2", null, asteroid.name));
+      }));
+    }
+  }]);
+
+  return App;
+}(_react.default.Component);
+
+exports.default = App;
 },{"react":"../node_modules/react/index.js"}],"../node_modules/parcel-bundler/src/builtins/bundle-url.js":[function(require,module,exports) {
 var bundleURL = null;
 
@@ -28416,7 +28496,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "37907" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45383" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,51 @@
 import React from "react"
 
-export default function App() {
-  return (
-    <div className='app__base asteroids'>
-      <h3>Reveal your approaches Oh! ye asteroids 🤣 </h3>
-    </div>
-  )
+// API Endpoint && key
+const asteroid_key = "7UpN204mxX9ABjxG3j8quPfoLak8N2WGyZBaY741"
+const asetroid_endpoint = `https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=${asteroid_key}`
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      asteroids: [],
+      isLoading: false,
+    }
+  }
+
+  // fetch data from NASA's NEoW API
+  componentDidMount() {
+    // get loading key
+    this.setState({
+      isLoading: true
+    })
+
+    fetch(asetroid_endpoint)
+      .then((response) => response.json)
+      .then((data) =>
+        this.setState({
+          asteroids: data.asteroids,
+        })
+      )
+  }
+
+  render() {
+    const { asteroids, isLoading } = this.state
+
+    // perform react conditional rendering
+    // by displaying either a loader or text indicating that data is being fetched
+    isLoading ? <p>Loading...</p> : 'err'
+
+    return (
+      <div className='app__base asteroids'>
+        <h3>Reveal your approaches Oh! ye asteroids 🤣 </h3>
+        {asteroids.map(asteroid => {
+          return (
+            <div key={asteroid.ObjectID}>
+              <h2>{asteroid.name}</h2>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 }
